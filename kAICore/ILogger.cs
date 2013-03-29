dc1d14dbfdc886f4cb804856892e01e674ef689f
thk123 <thekileyenator@gmail.com>
2013-03-29 13:15:04 +0000

    [Core][+] kAICore project created
    [Core][+] kAIObject class - all classes inherit so can log errors etc
    [Core][+] kAIILogger - an interface users can provide warnings, error reporting etc

---
+++ kAICore/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kAI.Core
+{
+    public interface kAIILogger
+    {
+        void LogMessage(string lMessage);
+        void LogWarning(string lWarning, params object[] lDetails);
+        void LogError(string lError, params object[] lDetails);
+        void LogCriticalError(string lError, params object[] lDetails);
+    }
+}
---
+++ kAICore/Object.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kAI.Core
+{
+    public abstract class kAIObject
+    {
+        static kAIILogger mGlobalLogger;
+        kAIILogger mLogger;
+
+        public static void SetLogger(kAIILogger lLogger);
+
+        public kAIObject()
+        {
+            mLogger = null;
+        }
+
+        public kAIObject(kAIILogger lLogger)
+        {
+            mLogger = lLogger;
+        }
+
+        public void LogMessage(string lMessage)
+        {
+            if (mLogger != null)
+            {
+                mLogger.LogMessage(GetType().ToString() + ": " + lMessage);
+            }
+
+            if (mGlobalLogger != null)
+            {
+                mLogger.LogMessage(GetType().ToString() + ": " + lMessage);
+            }
+        }
+
+        public void LogWarning(string lWarning, params object[] lDetails)
+        {
+            if (mLogger != null)
+            {
+                mLogger.LogWarning(GetType().ToString() + ": " + lWarning, lDetails);
+            }
+
+            if (mGlobalLogger != null)
+            {
+                mLogger.LogWarning(GetType().ToString() + ": " + lWarning, lDetails);
+            }
+        }
+
+        public void LogError(string lError, params object[] lDetails)
+        {
+            if (mLogger != null)
+            {
+                mLogger.LogError(GetType().ToString() + ": " + lError, lDetails);
+            }
+
+            if (mGlobalLogger != null)
+            {
+                mLogger.LogError(GetType().ToString() + ": " + lError, lDetails);
+            }
+        }
+
+        public void LogCriticalError(string lError, params object[] lDetails)
+        {
+            if (mLogger != null)
+            {
+                mLogger.LogCriticalError(GetType().ToString() + ": " + lError, lDetails);
+            }
+
+            if (mGlobalLogger != null)
+            {
+                mLogger.LogCriticalError(GetType().ToString() + ": " + lError, lDetails);
+            }
+        }
+    }
+}
