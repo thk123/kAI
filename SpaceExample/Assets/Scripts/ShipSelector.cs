a776010f0075b07ce13767a63fdf86a759536ec1
thk123 <thekileyenator@gmail.com>
2013-12-17 15:32:41 +0000

    [SPACE][+] Can select ships
    [SPACE][+] Camera shows which ship selected

---
+++ SpaceExample/Assets/Scripts/ShipSelector.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class ShipSelector : MonoBehaviour {
+
+	Camera selectionCamera;
+	
+	public SelectedCamera selectedCamera;
+
+	public Selectable selectedShip
+	{
+		get;
+		private set;
+	}
+
+	// Use this for initialization
+	void Start () {
+		selectionCamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(Input.GetMouseButtonUp(0))
+		{
+			print ("doing ray cast");
+			Ray selectionRay =  selectionCamera.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit info;
+
+
+			if(Physics.Raycast(selectionRay, out info, 1000.0f))
+			{
+				Selectable selectableObject = info.collider.gameObject.GetComponent<Selectable>();
+				if(selectableObject != null)
+				{
+					if(selectedShip != null)
+					{
+						selectedShip.Deselect();
+					}
+					selectedShip = selectableObject;
+					selectableObject.Select();
+
+					selectedCamera.SelectObject(selectedShip.gameObject);
+					return;
+				}
+			}
+
+			selectedCamera.DeselectObject();
+		}
+	}
+}
