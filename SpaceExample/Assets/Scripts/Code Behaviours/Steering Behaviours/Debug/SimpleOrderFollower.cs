cfc4eb5e7999b84027f9815c764c6d35bec397ca
thk123 <thekileyenator@gmail.com>
2014-01-20 17:14:42 +0000

    [SPACE][#] Moved the MoveTo code in to a code behaviour so can be used by the tool

---
+++ SpaceExample/Assets/Scripts/Code Behaviours/Steering Behaviours/Debug/SimpleOrderFollower.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using kAI.Core;
+
+public class SimpleOrderFollower : MonoBehaviour, IOrderReciever {
+
+    MoveTo mover;
+
+	// Use this for initialization
+	void Start () {
+        mover = new MoveTo();
+        mover.SetGlobal();
+        mover.ForceActivation();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mover.Update(Time.deltaTime, gameObject);
+	}
+
+    public void GiveOrder(Vector2 temp)
+    {
+        ((kAIDataPort<Vector2>)mover.GetPort("Target")).Data = temp;
+    }
+}
