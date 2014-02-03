6122fd2707ce95a072b99428f61a28ecd639e470
thk123 <thekileyenator@gmail.com>
2014-02-03 17:08:00 +0000

    [GROUND][+] More initial stuff
    [GROUND][+] Migrated useful scripts across
    [GROUND][+] Added camera controls
    [GROUND][+] Imported DLLs
    [CORE][#] Added ground example to export path

---
+++ GroundExample/Assets/Scripts/Game/CameraController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float cameraPanningSpeed = 0.5f;
+
+	Vector3 cameraPanLeft;
+	Vector3 cameraPanForward;
+
+	public bool locked;
+
+	// Use this for initialization
+	void Start () {
+		cameraPanLeft = -1 * transform.right;
+		cameraPanForward = transform.up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			locked = !locked;
+		}
+
+		if(!locked)
+		{
+			if(Input.mousePosition.x <= 10.0f)
+			{
+				transform.Translate(cameraPanningSpeed * cameraPanLeft, Space.World);
+			}
+
+
+			if(Input.mousePosition.x >= Screen.width - 10.0f )
+			{
+				transform.Translate(cameraPanningSpeed * -1 * cameraPanLeft, Space.World);
+			}
+
+			if(Input.mousePosition.y <= 10.0f)
+			{
+				transform.Translate(-1 * cameraPanningSpeed * cameraPanForward, Space.World);
+			}
+
+			if(Input.mousePosition.y >= Screen.height - 10.0f)
+			{
+				transform.Translate(cameraPanningSpeed * cameraPanForward, Space.World);
+			}
+
+			float scrollValue = Input.GetAxis("Mouse ScrollWheel");
+
+			if(scrollValue > 0.0f)
+			{
+                camera.fieldOfView = Mathf.Max(camera.fieldOfView - 1, 10.0f);
+			}
+			else if(scrollValue < 0.0f)
+			{
+                camera.fieldOfView = Mathf.Min(camera.fieldOfView + 1, 130.0f);
+			}
+		}
+
+	}
+
+	void OnGUI()
+	{
+		if(locked)
+		{
+			GUI.TextArea(new Rect(10, 10, 100, 20), "Locked");
+		}
+	}
+
+}
---
+++ GroundExample/Assets/Scripts/Game/Selectable.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selectable : MonoBehaviour {
+
+	bool selected;
+
+	Rect boxRectangle;
+
+	MeshRenderer mesh;
+
+	// Use this for initialization
+	void Start () {
+		selected = false;
+		mesh = GetComponentInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(selected)
+		{
+			Bounds bounds = mesh.bounds;
+			Vector3 maxPoint = Camera.main.WorldToScreenPoint(bounds.max);
+			Vector3 minPoint = Camera.main.WorldToScreenPoint(bounds.min);
+
+			boxRectangle = new Rect(maxPoint.x, maxPoint.z, maxPoint.x - minPoint.x, maxPoint.z - minPoint.z);
+		}
+	}
+
+	void OnGUI()
+	{
+		if(selected)
+		{
+			
+			GUI.Box(boxRectangle, new GUIContent());
+		}
+	}
+
+	public void Select()
+	{
+		selected = true;
+	}
+
+	public void Deselect()
+	{
+		selected = false;
+	}
+}
