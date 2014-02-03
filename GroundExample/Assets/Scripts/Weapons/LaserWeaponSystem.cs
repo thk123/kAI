83550aadd16991a21fa7dba2e87dd8f09b4d440a
thk123 <thekileyenator@gmail.com>
2014-02-03 23:07:14 +0000

    [GROUND][+] Moved weapon system across to ground

---
+++ GroundExample/Assets/Scripts/Weapons/LaserWeaponSystem.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserWeaponSystem : MonoBehaviour, IWeaponSystem {
+
+    public float maxDistance;
+    public float firingDuration;
+    public float damagePerSecond;
+
+
+    LineRenderer laserRenderer;
+
+    public Transform laserStartPoint;
+
+    float currentFireDuration;
+
+	// Use this for initialization
+	void Start () {
+        currentFireDuration = firingDuration;
+        laserRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(currentFireDuration >= firingDuration)
+        {
+            laserRenderer.SetVertexCount(0);
+        }
+        else
+        {
+            UpdateLaser();
+            currentFireDuration += Time.deltaTime;
+        }
+	}
+
+    void UpdateLaser()
+    {
+        RaycastHit result;
+
+        if (Physics.Raycast(laserStartPoint.position, laserStartPoint.forward, out result, maxDistance))
+        {
+            laserRenderer.SetVertexCount(2);
+            laserRenderer.SetPosition(0, laserStartPoint.position);
+            laserRenderer.SetPosition(1, result.point);
+
+            HealthBehaviour health = result.collider.GetComponent<HealthBehaviour>();
+            if (health != null)
+            {
+                health.ApplyDamage(damagePerSecond * Time.deltaTime, transform.forward);
+            }
+
+        }
+        else
+        {
+            laserRenderer.SetVertexCount(2);
+            laserRenderer.SetPosition(0, laserStartPoint.position);
+            laserRenderer.SetPosition(1, laserStartPoint.position + (laserStartPoint.forward * maxDistance));
+        }
+        /*RaycastHit2D result = Physics2D.Raycast(laserStartPoint.position, transform.forward, maxDistance);
+
+        if (result)
+        {
+            laserRenderer.SetVertexCount(2);
+            laserRenderer.SetPosition(0, laserStartPoint.position);
+            laserRenderer.SetPosition(1, new Vector3(result.point.x, result.point.y, transform.position.z));
+
+            HealthBehaviour health = result.collider.GetComponent<HealthBehaviour>();
+            if(health != null)
+            {
+                health.ApplyDamage(damagePerSecond * Time.deltaTime, transform.right);  
+            }
+
+        }
+        else
+        {
+            laserRenderer.SetVertexCount(2);
+            laserRenderer.SetPosition(0, laserStartPoint.position);
+            laserRenderer.SetPosition(1, laserStartPoint.position + (laserStartPoint.right * maxDistance));
+        }*/
+    }
+
+    public void Fire()
+    {
+        currentFireDuration = 0.0f;        
+    }
+
+
+    public void SetSpawnPoint(Transform spawn)
+    {
+        laserStartPoint = spawn;
+    }
+}
---
+++ GroundExample/Assets/Scripts/Weapons/WeaponSystemManager.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using kAI.Core;
+
+public interface IWeaponSystem
+{
+    void Fire();
+
+    void SetSpawnPoint(Transform spawn);
+}
+
+public class WeaponSystemManager : MonoBehaviour {
+
+    public GameObject MainWeaponObject;
+
+    public GameObject MissileWeaponObject;
+
+    public Transform SpawnPoint;
+
+    public IWeaponSystem MainWeapon
+    {
+        get
+        {
+            if (MainWeaponObject != null)
+            {
+                return (IWeaponSystem)MainWeaponObject.GetComponent(typeof(IWeaponSystem));
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public IWeaponSystem MissileWeapon
+    {
+        get
+        {
+            if (MissileWeaponObject != null)
+            {
+                return (IWeaponSystem)MissileWeaponObject.GetComponent(typeof(IWeaponSystem));
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	    if(MainWeapon != null)
+        {
+            MainWeapon.SetSpawnPoint(SpawnPoint);
+        }
+
+        if(MissileWeapon != null)
+        {
+            MissileWeapon.SetSpawnPoint(SpawnPoint);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+    
+}
+
+public static class WeaponFunctions
+{
+    public static float DistanceToTarget(GameObject self, GameObject target)
+    {
+        Debug.Log("DistanceToTarget");
+        return (self.transform.position - target.transform.position).magnitude;
+    }
+
+    public static float AngleToTarget(GameObject self, GameObject target)
+    {
+        if(target != null)
+        {
+            Vector2 vectorToTarget = (target.transform.position - self.transform.position);
+            float answer = Vector2.Angle(self.transform.right, vectorToTarget);
+
+            Vector3 cross = Vector3.Cross(self.transform.right, vectorToTarget);
+
+            if (cross.z < 0.0f)
+            {
+                answer = -answer;
+            }
+
+
+            Debug.Log("AngleToTarget: " + answer);
+            return answer;
+        }
+        else
+        {
+            return 0.0f;
+        }
+    }
+}
+
+public class FireWeaponBehaviour : kAICodeBehaviour
+{
+    bool shouldFire;
+    kAIDataPort<float> gunArc;
+    public FireWeaponBehaviour()
+    {
+        kAITriggerPort fireTrigger = new kAITriggerPort("Fire", kAIPort.ePortDirection.PortDirection_In);
+        fireTrigger.OnTriggered += fireTrigger_OnTriggered;
+         
+        AddExternalPort(fireTrigger);
+
+        gunArc = new kAIDataPort<float>("GunArc", kAIPort.ePortDirection.PortDirection_Out);
+        AddExternalPort(gunArc);
+    }
+
+    void fireTrigger_OnTriggered(kAIPort lSender)
+    {
+        LogMessage("Fire trigger");
+        shouldFire = true;
+    }
+
+    protected override void InternalUpdate(float lDeltaTime, object lUserData)
+    {
+        if(shouldFire)
+        {
+            GameObject shipObject = lUserData as GameObject;
+
+            if(shipObject != null)
+            {
+                WeaponSystemManager weaponSystem = shipObject.GetComponent<WeaponSystemManager>();
+                gunArc.Data = 45.0f; //weaponSystem.MainWeapon.
+
+                weaponSystem.MainWeapon.Fire();
+
+                shouldFire = false;
+            }
+
+        }
+    }
+}
+
