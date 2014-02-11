ba6565c44d60fa03877646e539dd4cc0f23ab826
thk123 <thekileyenator@gmail.com>
2014-02-11 15:21:45 +0000

    [CORE][+] Added boolean functions

--- kAICore/FunctionNode.cs
+++ kAICore/FunctionNode.cs
@@ -341,7 +341,6 @@ namespace kAI.Core
 
         public static T AOrBChooser<T>(bool value, T trueValue, T falseValue)
         {
-            kAIObject.LogMessage(null, "Checking " + value + " to compute either " + trueValue.ToString() + " or " + falseValue.ToString());
             T result = value ? trueValue : falseValue;
            //kAIObject.LogMessage(null, "Checking " + value + " getting " + result.ToString());
 
@@ -358,6 +357,20 @@ namespace kAI.Core
             return default(T);
         }
 
+        public static bool And(bool entry1, bool entry2)
+        {
+            return entry1 && entry2;
+        }
+
+        public static bool Or(bool entry1, bool entry2)
+        {
+            return entry1 || entry2;
+        }
+
+        public static bool Not(bool entry)
+        {
+            return !entry;
+        }
     }
 
     /// <summary>
