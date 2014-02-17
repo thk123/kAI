282b2a99c2412065b03ff2539cff6d096c3f5f0a
thk123 <thekileyenator@gmail.com>
2014-02-17 21:37:20 +0000

    [GROUND][+] Created simple flee behaviour that just heads to the origin
    [GROUND][+] Made the SelfPreservation behaviour cause the ship to flee if health <0.5

---
+++ GroundExample/Assets/Scripts/CodeBehaviours/Flee.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Flee : CodeBehaviourTester
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
+
+public class FleeBehaviour : kAIUnityAIBehaviour
+{
+    public FleeBehaviour()
+        :base()
+    {
+    }
+    public override void UnityInternalUpdate(float lDeltaTime, GameObject lShipObject)
+    {
+        
+    }
+
+    public static Vector3 GetFleePosition(GameObject self)
+    {
+        return Vector3.zero;
+    }
+}
